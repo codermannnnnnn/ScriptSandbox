9fa943100ec2beb92fba9ecb970082f3191b41b0
codermannnnnnn <164600483+codermannnnnnn@users.noreply.github.com>
2024-04-27 13:35:22 +1000

    Update script_run.js

--- script_run.js
+++ script_run.js
@@ -1,14 +1,18 @@
 function updateHtml() {
-  (document.getElementsByTagName("iframe")[0].id=="js"?document.getElementsByTagName("iframe")[1]:document.getElementsByTagName("iframe")[0]).contentWindow.location.reload();
-  (document.getElementsByTagName("iframe")[0].id=="js"?document.getElementsByTagName("iframe")[1]:document.getElementsByTagName("iframe")[0]).contentDocument.body.innerHTML+=document.getElementById("html").value;
+  (document.getElementsByTagName("iframe")[0].id=="js_output_iframe"?document.getElementsByTagName("iframe")[1]:document.getElementsByTagName("iframe")[0]).contentWindow.location.reload();
+  (document.getElementsByTagName("iframe")[0].id=="js_output_iframe"?document.getElementsByTagName("iframe")[1]:document.getElementsByTagName("iframe")[0]).contentDocument.body.innerHTML+=document.getElementById("html").value;
 }
 
 function runJs() {
   var result = "";
   try {
-    result = eval(document.getElement)
-  }
-  (document.getElementsByTagName("iframe")[1].id=="html"?document.getElementsByTagName("iframe")[0]:document.getElementsByTagName("iframe")[1]).contentWindow.location.reload();
-  (document.getElementsByTagName("iframe")[1].id=="html"?document.getElementsByTagName("iframe")[0]:document.getElementsByTagName("iframe")[1]).contentDocument.body.innerHTML+="Output: ";
-  
+    result = eval(document.getElementById("js"));
+  } catch (error) {
+    result = `${error.name}: ${error.message}`;
+  };
+  (document.getElementsByTagName("iframe")[1].id=="html_iframe"?document.getElementsByTagName("iframe")[0]:document.getElementsByTagName("iframe")[1]).contentWindow.location.reload();
+  (document.getElementsByTagName("iframe")[1].id=="html_iframe"?document.getElementsByTagName("iframe")[0]:document.getElementsByTagName("iframe")[1]).contentDocument.body.innerHTML+="Output: ";
+  (document.getElementsByTagName("iframe")[1].id=="html_iframe"?document.getElementsByTagName("iframe")[0]:document.getElementsByTagName("iframe")[1]).contentDocument.body.innerHTML+=result;
 }
+export default updateHtml;
+export default runJs;
