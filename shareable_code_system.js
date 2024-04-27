395bec736426e237ad11ecfc16031a1d3ff687eb
codermannnnnnn <164600483+codermannnnnnn@users.noreply.github.com>
2024-04-28 05:20:48 +1000

    Create shareable_code_system.js
    w.i.p

---
+++ shareable_code_system.js
@@ -0,0 +1,16 @@
+function shareCodeLink(type, code) {
+  p = confirm("Copy link?");
+  if (p == true) {
+    navigator.clipboard.writeText("https://codermannnnnnn.github.io/ScriptSandbox/#codecrypt-"+btoa(escape(btoa(type)))+":"+btoa(escape(btoa(code))));
+    alert("Copied to clipboard!")
+  } else {
+    alert("Ok")
+  }
+}
+
+function sharedCodeSet() {
+  p = location.href.replace("https://codermannnnnnn.github.io/ScriptSandbox/#codecrypt-","");
+  var type, code;
+  p.split(":").forEach((element,index)=>{if(index==0){type=atob(unescape(atob(element)))}else{code=atob(unescape(atob(element)))}});
+  document.getElementsFromTagName("input");
+}
